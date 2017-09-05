553b8cd9a4c01b7aa9984194f2aee8d03381c303
kejian <hellokejian@163.com>
2017-09-05 21:31:30 +0800

    create 09

--- basic/09String.py
+++ basic/09String.py
@@ -1,10 +1,80 @@
-from math import *
+# def pringstr(str):
+#     index = 0
+#     while index < len(str):
+#         print(str[index])
+#         index += 1
+#
+#
+# pringstr("kejian")
+#
+# def reverse(str):
+#     index = len(str)-1
+#     while index >= 0:
+#         print(str[index])
+#         index -= 1
+#
+# reverse("kejian")
 
+# def forUseage(str):
+#     for char in str:
+#         print(char)
+#
+# forUseage("kejian")
 
-def pringStr(str):
-    index = 0
-    while (index < len(str)):
-       print(str[index])
-       index += 1
+# def addLetter():
+#     prefixes = "JKLMNOPQ"
+#     suffux = "ack"
+#     for letter in prefixes:
+#         if letter == 'O' or letter == 'Q':
+#             print(letter + 'u' + suffux)
+#         else:
+#             print(letter + suffux)
+#
+#
+# addLetter()
 
-pringStr("kejian")
+# def slice(str):
+#     # s[m,n]从位置m开始,切到n位置,但不包括下标为n的那个元素
+#     result = str[1:len(str)-1]
+#     print(result)
+#
+# slice("kejian")
+
+# print("kejian"[:])  # 全部切下来
+
+# def find(word, letter, begin):
+#     if begin >= len(word):
+#         return -1
+#     index = begin
+#     while index < len(word):
+#         if word[index] == letter:
+#             return index
+#         index += 1
+#     return -1
+#
+#
+# print(find("administrator", 'i', 4))
+
+# print("kejian".upper())
+
+# index = "kejian".find('e')
+# print(index)
+
+# print("administrator".find("st", 7, 9))
+
+# print("to be honest, i am very pleased to see you".count("to", 5, 7))
+
+"""strip"""
+# print("  kejian ".strip())
+
+""" 新的代码注释,学到了吧,哈哈"""
+
+"""replace"""
+# print("AppleStore".replace('Apple', "Oppo"))
+
+"""in"""
+# print("mi" in 'administrator')
+
+"""字符串的关系操作符"""
+print("apple" < "orange")
+print("Apple" < "apple")
---
+++ basic/10stringPractice.py
@@ -0,0 +1,21 @@
+"""指定步长切割"""
+
+# def is_palindrome(str):
+#     print(str[0:len(str):1])
+#     print(str[::-1])
+#     return str[0:len(str):1] == str[::-1]
+#
+#
+# print(is_palindrome("abccba"))
+
+"""ROT13编码加密"""
+
+# def rotate_word(word, num):
+#     newchar = ''
+#     for char in word:
+#         newchar += chr(ord(char) + num)
+#     return newchar
+#
+#
+# print(rotate_word("cheer", 7))
+
