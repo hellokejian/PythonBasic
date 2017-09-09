ae5d6648d8ce829cf23cd6ae8686eaa737b91279
kejian <hellokejian@163.com>
2017-09-09 20:47:01 +0800

    ...

--- basic/13CarTalk.py
+++ basic/13CarTalk.py
@@ -5,7 +5,7 @@
 #         return False
 #     return cartalk1(str[2:])
 
-def findtwice(str):
+def findthird(str):
     if len(str) < 6:
         return False
     index = 0
@@ -25,8 +25,76 @@ def findtwice(str):
 def cartalk1():
     fin = open("D:/WorkSpace(kejian)/Pycharm/PythonBasic/basic/12word.txt")
     for line in fin:
-        if findtwice(line):
+        if findthird(line):
             print(line)
 
 
-cartalk1()
+# cartalk1()
+
+def findReverse1():
+    num = 100000
+    while num < 999999:
+        num_str = str(num)
+        index = len(num_str)
+        i = 0
+        j = index - 1
+        couples = 0
+        while j > i:
+            if couples >= 2:
+                print(num)
+            if num_str[i] == num_str[j]:
+                couples += 1
+                i += 1
+                j -= 1
+            else:
+                j -= 1
+                couples = 0
+                break
+        couples = 0
+        i = 0
+        j = index - 1
+        while i < j:
+            if num_str[i] == num_str[j]:
+                couples += 1
+                i += 1
+                j -= 1
+            else:
+                i += 1
+                couples = 0
+                break
+            if couples == 2:
+                print(num)
+        num += 1
+
+
+def findReverse():
+    num = 100000
+    while num <= 999999:
+        num_str = str(num)
+        i = 0
+        while i < 2:
+            j = i + 3
+            while j < 6:
+                if is_Reversed(num_str[i:j + 1]):
+                    print(num_str)
+                    break
+                else:
+                    j += 1
+            i += 1
+        num += 1
+
+
+def is_Reversed(str):
+    length = len(str)
+    index = 0
+    while index <= length / 2:
+        if str[index] != str[length - index - 1]:
+            return False
+        index += 1
+    return True
+
+    # print(is_Reversed("sksd"))
+    # print("kejian"[0:6])
+
+
+findReverse()
